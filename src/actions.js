a9f4afb5fcb76822b2f63fefc2f6ebf796251ea0
Kost9in <Kost9inbi4@mail.ru>
2017-04-03 03:59:50 +0300

    Add build to gh-pages.

--- src/actions.js
+++
@@ -1,3 +0,0 @@
-export const chooseBook = bookId => ({ type: 'CHOOSE_BOOK', bookId });
-export const chooseData = data => ({ type: 'CHOOSE_DATA', data });
-export const loadData = (type, data) => ({ type: `${type}_LOAD_DATA`, data });
--- src/components/content/BookHistory.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default ({ history, readers }) => (
-    <ul>
-        {history.map(({ date, readerId }, idx) => (<li key={idx}>{date}: {readers[readerId]}</li>))}
-    </ul>
-);
